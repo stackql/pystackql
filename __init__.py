4b4049b2a272d1c071933260b81dfcb7e0a2c7a4
jeffreyaven <javen@avensolutions.com>
2022-02-05 10:52:23 +1100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,51 @@
+import sys, subprocess, platform
+
+class StackQL:
+	def __init__(self, **kwargs):
+		plat = platform.system()
+		defaultbin = r'stackql'
+		if plat == 'Windows':
+			defaultbin = r'stackql.exe'
+		# stackql binary
+		self.exe = kwargs.get('exe')
+		if self.exe is not None:
+			self.exe = kwargs.get('exe')
+		else:
+			self.exe = defaultbin
+		self.params = ['exec', '--output', 'json']			
+		# authentication method
+		self.keyfilepath = kwargs.get('keyfilepath')
+		if self.keyfilepath is not None:
+			self.params.append('--keyfilepath')
+			self.params.append(self.keyfilepath)		
+		# specify dbfilepath
+		self.dbfilepath = kwargs.get('dbfilepath')
+		if self.dbfilepath is not None:
+			self.params.append('--dbfilepath')
+			self.params.append(self.dbfilepath)
+			
+	def execute(self, query):
+		local_params = self.params
+		local_params.insert(1, query)
+		try:
+			iqlPopen = subprocess.Popen([self.exe] + local_params,
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			output = iqlPopen.stdout.read()
+			iqlPopen.terminate()
+		except:
+			e = sys.exc_info()[0]
+			print("ERROR %s %s" % (str(e), e.__doc__))
+			output = None
+		return output
+		
+	def version(self):
+		try:
+			iqlPopen = subprocess.Popen([self.exe] + ["--version"],
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			output = iqlPopen.stdout.read()
+			iqlPopen.terminate()
+		except:
+			e = sys.exc_info()[0]
+			print("ERROR %s %s" % (str(e), e.__doc__))
+			output = None
+		return output		
