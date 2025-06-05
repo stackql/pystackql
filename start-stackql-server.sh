6e3b851884359afb3974ad341b9846c70f9b4773
Jeffrey Aven <javen@stackql.io>
2025-06-05 11:25:44 +1000

    ci update

--- start-stackql-server.sh
+++ start-stackql-server.sh
@@ -1,7 +1,6 @@
 # start server if not running
 echo "checking if server is running"
 if [ -z "$(ps | grep stackql)" ]; then
-    echo "starting server with registry: $REG"
     nohup ./stackql -v --pgsrv.port=5444 srv &
     sleep 5
 else
